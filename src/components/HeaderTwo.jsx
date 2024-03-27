8ddc6b6fea522b8d04ba71c4547ad075df057736
Nowsath04 <nowsathnichu@gmail.com>
2024-03-27 20:27:06 +0530

    added

--- src/components/HeaderTwo.jsx
+++ src/components/HeaderTwo.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./HeaderTwo.css";
-import logo_img from "../images/raiden rewamp logo.png";
+import logo_img from "../images/raiden trade mark logo-min-1.png";
 const HeaderTwo = () => {
   const [active, setActive] = useState(false);
   const [scroll, setScroll] = useState(false);
