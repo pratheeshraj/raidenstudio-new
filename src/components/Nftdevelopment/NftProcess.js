feff625fe109bef8952f71e33a40a7e7668b65f7
Nowsath04 <nowsathnichu@gmail.com>
2024-03-27 19:07:58 +0530

    added

--- src/components/Nftdevelopment/NftProcess.js
+++ src/components/Nftdevelopment/NftProcess.js
@@ -1,18 +1,138 @@
-import React from 'react'
-import "./NftProcess.css"
-
+import React, { useEffect } from 'react';
+import "./NftProcess.css";
 
 function NftProcess() {
+    useEffect(() => {
+        const qs = (selector, all = false) => {
+            return all ? document.querySelectorAll(selector) : document.querySelector(selector);
+        };
+
+        const sections = qs('.nft_section', true);
+        const nft_timeline = qs('.nft_timeline');
+        const line = qs('.nft_line');
+
+        if (line) {
+            line.style.bottom = `calc(100% - 20px)`;
+            let prevScrollY = window.scrollY;
+            let up, down;
+            let full = false;
+            let set = 0;
+            const targetY = window.innerHeight * 0.8;
+
+            function scrollHandler(e) {
+                const {
+                    scrollY
+                } = window;
+                up = scrollY < prevScrollY;
+                down = !up;
+                const timelineRect = nft_timeline.getBoundingClientRect();
+                const lineRect = line.getBoundingClientRect(); //CONST LINEHEIGHT = lineRect.bottom - lineRect.top
+
+                const dist = targetY - timelineRect.top
+                console.log(dist);
+
+                if (down && !full) {
+                    set = Math.max(set, dist);
+                    line.style.bottom = `calc(100% - ${set}px)`
+                }
+
+                if (dist > nft_timeline.offsetHeight + 50 && !full) {
+                    full = true;
+                    line.style.bottom = `-50px`
+                }
+
+                sections.forEach(item => {
+                    //console.log(items);
+                    const rect = item.getBoundingClientRect();
+
+                    if (rect.top + item.offsetHeight / 5 < targetY) {
+                        item.classList.add('show-me')
+                    }
+                });
+
+                prevScrollY = window.scrollY;
+            }
+
+            scrollHandler();
+            line.style.display = 'block';
+            window.addEventListener('scroll', scrollHandler);
+
+            return () => {
+                window.removeEventListener('scroll', scrollHandler);
+            };
+        }
+    }, []); // Empty dependency array to run effect only once after mount
+
     return (
         <section className="faq-section three" style={{ background: "#00151E" }}>
             <div className="auto-container">
                 <div className="row">
-                    <h2 style={{ textAlign: "center", color: "white" }}>Employing AI Development Services in <br />Multiple <span className='theme-color'> Industries</span></h2>
-                    <p style={{ textAlign: "center", color: "white" }}>Our AI development team is up to date on the newest technology trends, and they continue to experiment with new AI PoCs (Proof of Concepts) to provide cutting-edge AI development solutions with novel ideas.</p>
+                    <h2 style={{ textAlign: "center", color: "white" }}>Our NFT Development<span className='theme-color'> Process</span></h2>
+                    <p style={{ textAlign: "center", color: "white" }}>Dive into the world of NFTs with our end-to-end development process
 
+                        Detailed market research along with comprehensive analysis of the NFT project idea ensures your success by outlining its goal, target audience, and unique selling factors.
+                    </p>
+                    <div className="nft_timeline">
+                        <div className="nft_line"></div>
+                        <div className="nft_section">
+                            <div className="bead">1</div>
+                            <div className="nft_content">
+                                <h6>Planning</h6>
+                                <p>
+                                    We take you through all of the processes involved in designing and developing digital assets for your NFT project's process.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="nft_section">
+                            <div className="bead">2</div>
+                            <div className="nft_content">
+                                <h6>Wireframing</h6>
+                                <p>
+                                    We create a solid framework to map out the vision, which covers blockchain, token minting, marketplace integration, and other elements. Additionally, we sketch up the entire NFT project strategy.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="nft_section">
+                            <div className="bead">3</div>
+                            <div className="nft_content">
+                                <h6>UI/UX</h6>
+                                <p>
+                                    We prioritize the success of the NFT project by focusing on the user interface and experience.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="nft_section">
+                            <div className="bead">4</div>
+                            <div className="nft_content">
+                                <h6>Development</h6>
+                                <p>
+                                    By working with the technical team, we can speed up the NFT project's development and assure the best outcomes.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="nft_section">
+                            <div className="bead">5</div>
+                            <div className="nft_content">
+                                <h6>QA</h6>
+                                <p>
+                                    We guarantee an in-depth quality analysis and testing of a bug-free NFT application with no coding errors.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="nft_section">
+                            <div className="bead">6</div>
+                            <div className="nft_content">
+                                <h6>Launch</h6>
+                                <p>
+                                    We launch your brand-new NFT project on all platforms, making it available to users and allowing your business to start generating revenue.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </section>)
+        </section>
+    );
 }
 
-export default NftProcess
+export default NftProcess;
