ae8541396280abb077649833bc3b0be4cd7d8e2b
Nowsath04 <nowsathnichu@gmail.com>
2024-03-28 17:33:43 +0530

    added

--- src/components/Nftdevelopment/NftFeatures.jsx
+++ src/components/Nftdevelopment/NftFeatures.jsx
@@ -10,59 +10,59 @@ function NftFeatures() {
                     <p style={{ textAlign: "center", color: "black" }}>Our Nft development team is up to date on the newest technology trends, and they continue to experiment with new Nft PoCs (Proof of Concepts) to provide cutting-edge Nft development solutions with novel ideas.</p>
                     <div className='feature_div'>
                         <div className='feature_card'>
-                            <img src='/assets/img/development-solution/gasw@0.3x.png' />
+                            <img src='/assets/img/nftdevfeatu/nftwallet.svg' />
                             <div>Wallet integration</div>
                         </div>
                         <div className='feature_card'>
-                            <img src='/assets/img/development-solution/csd@0.3x.png' />
+                            <img src='/assets/img/nftdevfeatu/multichain.svg' />
                             <div>Multi-chain Compatibility</div>
                         </div>
                         <div className='feature_card'>
-                            <img src='/assets/img/development-solution/deep-learning (1)@0.3x.png' />
+                            <img src='/assets/img/nftdevfeatu/management.svg' />
                             <div>User management</div>
                         </div>
                         <div className='feature_card'>
-                            <img src='/assets/img/development-solution/ocr@0.3x.png' />
+                            <img src='/assets/img/nftdevfeatu/nftstore.svg' />
                             <div>Storefront</div>
                         </div>
                         <div className='feature_card'>
-                            <img src='/assets/img/development-solution/automation@0.3x.png' />
+                            <img src='/assets/img/nftdevfeatu/ipfs.svg' />
                             <div>IPFS NFT Storage</div>
                         </div>
                         <div className='feature_card'>
-                            <img src='/assets/img/development-solution/natural-language-processing@0.3x.png' />
+                            <img src='/assets/img/nftdevfeatu/tokanization.svg' />
                             <div>Tokenization</div>
                         </div>
                         <div className='feature_card'>
-                            <img src='/assets/img/development-solution/playing bot@0.3x.png' />
+                            <img src='/assets/img/nftdevfeatu/royalty.svg' />
                             <div>Global Royalty Standard</div>
                         </div>
                         <div className='feature_card'>
-                            <img src='/assets/img/development-solution/ai solution@0.3x.png' />
+                            <img src='/assets/img/nftdevfeatu/transaction.svg' />
                             <div>Gasless Transactions</div>
                         </div>
                         <div className='feature_card'>
-                            <img src='/assets/img/development-solution/costumer-service@0.3x.png' />
+                            <img src='/assets/img/nftdevfeatu/ech.svg' />
                             <div>ERC-721 & 1155 Support</div>
                         </div>
                         <div className='feature_card'>
-                            <img src='/assets/img/development-solution/machine-learning@0.3x.png' />
+                            <img src='/assets/img/nftdevfeatu/exportnft.svg' />
                             <div>Export NFTs</div>
                         </div>
                         <div className='feature_card'>
-                            <img src='/assets/img/development-solution/natural-language-processing@0.3x.png' />
+                            <img src='/assets/img/nftdevfeatu/PaymentGatewayIntegration.svg' />
                             <div>Payment Gateway Integration</div>
                         </div>
                         <div className='feature_card'>
-                            <img src='/assets/img/development-solution/natural-language-processing@0.3x.png' />
+                            <img src='/assets/img/nftdevfeatu/erc.svg' />
                             <div>ERP & CRM integration</div>
                         </div>
                         <div className='feature_card'>
-                            <img src='/assets/img/development-solution/natural-language-processing@0.3x.png' />
+                            <img src='/assets/img/nftdevfeatu/blockchainintegration.svg' />
                             <div>Blockchain Integration</div>
                         </div>
                         <div className='feature_card'>
-                            <img src='/assets/img/development-solution/natural-language-processing@0.3x.png' />
+                            <img src='/assets/img/nftdevfeatu/nftmeta.svg' />
                             <div>NFT MetaData</div>
                         </div>
                     </div>
--- src/components/Nftdevelopment/NftIndustries.jsx
+++ src/components/Nftdevelopment/NftIndustries.jsx
@@ -6,9 +6,46 @@ function NftIndustries() {
         <section className="faq-section three" style={{ background: "#00151E" }}>
             <div className="auto-container">
                 <div className="row">
-                    <h2 style={{ textAlign: "center", color: "white" }}>Employing AI Development Services in <br />Multiple <span className='theme-color'> Industries</span></h2>
+                    <h2 style={{ textAlign: "center", color: "white" }}>Employing AI Development Services in <br />Multiple <span className='theme-color'> industries</span></h2>
                     <p style={{ textAlign: "center", color: "white" }}>Our AI development team is up to date on the newest technology trends, and they continue to experiment with new AI PoCs (Proof of Concepts) to provide cutting-edge AI development solutions with novel ideas.</p>
                     <div className='feature_div'>
+                        <div className='feature_card idustry'>
+                            <img src='/assets/img/nftindustries/art.svg' />
+                            <div>Art</div>
+                        </div>
+                        <div className='feature_card idustry'>
+                            <img src='/assets/img/nftindustries/music.svg' />
+                            <div>Music</div>
+                        </div>
+                        <div className='feature_card idustry'>
+                            <img src='/assets/img/nftindustries/DIGITAL.SVG' />
+                            <div>Digital Collectibles
+                            </div>
+                        </div>
+                        <div className='feature_card idustry'>
+                            <img src='/assets/img/nftindustries/fashion.svg' />
+                            <div>Fashion
+                            </div>
+                        </div>
+                        <div className='feature_card idustry'>
+                            <img src='/assets/img/nftindustries/metaverse.svg' />
+                            <div>Metaverse
+                            </div>
+                        </div>
+                        <div className='feature_card idustry'>
+                            <img src='/assets/img/nftindustries/automotive.svg' />
+                            <div>Automotive
+                            </div>
+                        </div>
+                        <div className='feature_card idustry'>
+                            <img src='/assets/img/nftindustries/oilandgas.svg' />
+                            <div>Oil & Gas
+                            </div>
+                        </div>
+                        <div className='feature_card idustry'>
+                            <img src='/assets/img/nftindustries/manufacturing.svg' />
+                            <div>Manufacturing</div>
+                        </div>
                         <div className='feature_card idustry'>
                             <img src='/assets/img/industries/healthcare@0.3x.png' />
                             <div>Healthcare</div>
@@ -36,43 +73,6 @@ function NftIndustries() {
                         <div className='feature_card idustry'>
                             <img src='/assets/img/industries/mortarboard@0.3x.png' />
                             <div>Eduction</div>
-                        </div>
-                        <div className='feature_card idustry'>
-                            <img src='/assets/img/industries/mortarboard@0.3x.png' />
-                            <div>Art</div>
-                        </div>
-                        <div className='feature_card idustry'>
-                            <img src='/assets/img/industries/mortarboard@0.3x.png' />
-                            <div>Music</div>
-                        </div>
-                        <div className='feature_card idustry'>
-                            <img src='/assets/img/industries/mortarboard@0.3x.png' />
-                            <div>Digital Collectibles
-                            </div>
-                        </div>
-                        <div className='feature_card idustry'>
-                            <img src='/assets/img/industries/mortarboard@0.3x.png' />
-                            <div>Fashion
-                            </div>
-                        </div>
-                        <div className='feature_card idustry'>
-                            <img src='/assets/img/industries/mortarboard@0.3x.png' />
-                            <div>Metaverse
-                            </div>
-                        </div>
-                        <div className='feature_card idustry'>
-                            <img src='/assets/img/industries/mortarboard@0.3x.png' />
-                            <div>Automotive
-                            </div>
-                        </div>
-                        <div className='feature_card idustry'>
-                            <img src='/assets/img/industries/mortarboard@0.3x.png' />
-                            <div>Oil & Gas
-                            </div>
-                        </div>
-                        <div className='feature_card idustry'>
-                            <img src='/assets/img/industries/restaurant@0.3x.png' />
-                            <div>Manufacturing</div>
                         </div>
                     </div>
                 </div>
