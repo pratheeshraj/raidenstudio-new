34a4cd32a8e22d3b5c0a6435c6b65ce32431b5cd
Nowsath04 <nowsathnichu@gmail.com>
2024-04-08 21:30:31 +0530

    added

--- src/components/MetaverseCasino/ChoiceOfGames.jsx
+++ src/components/MetaverseCasino/ChoiceOfGames.jsx
@@ -82,10 +82,10 @@ function ChoiceOfGames() {
                 <h2 className="auto-container">Choice of Games</h2>
                 <p className='choicegame_main_p'>Discover a diverse array of thrilling games awaiting you at VirtualVegas. From classic casino favorites to innovative virtual reality experiences, our platform offers something for every type of player. </p>
                 <div className="coice-link-div">
-                    <div className={classic ? "Choice_active" : "Choice_active_div"} onClick={ClassicHandle}>Classic Casino Games</div>
+                    <div className={classic ? "Choice_active" : "Choice_active_div"} onClick={ClassicHandle}>Classic Casino</div>
                     <div className={slots ? "Choice_active" : "Choice_active_div"} onClick={SlotHandle}>Slots</div>
-                    <div className={table ? "Choice_active" : "Choice_active_div"} onClick={TableHandle}>Table Games</div>
-                    <div className={specialty ? "Choice_active" : "Choice_active_div"} onClick={specHandle}>Specialty Games</div>
+                    <div className={table ? "Choice_active" : "Choice_active_div"} onClick={TableHandle}>Table</div>
+                    <div className={specialty ? "Choice_active" : "Choice_active_div"} onClick={specHandle}>Specialty</div>
                 </div>
                 {
                     classic ? <div className="auto-container cogini_slider">
