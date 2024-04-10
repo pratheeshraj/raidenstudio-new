5e23e2c291ea639a35202223500468b16a27d664
pratheeshraj <rajeshnsk5@gmail.com>
2024-04-10 19:23:42 +0530

    add

--- src/App.js
+++ src/App.js
@@ -42,6 +42,10 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import MetaverseCasino from './pages/MetaverseCasino';
 import VRdevelopment from "./pages/VRdevelopment";
+import MetaverseRoyal from "./pages/MetaverseRoyal";
+
+import ARdevelopment from "./pages/ARdevelopment";
+
 
 
 function App() {
@@ -90,6 +94,11 @@ function App() {
         <Route exact path="/raiden-cogni" element={<RaidenCogni />} />
         <Route exact path="/metaverse-casino" element={<MetaverseCasino />} />
         <Route exact path="/vr-development" element={<VRdevelopment />} />
+
+        <Route exact path="/metaverse-royal" element={<MetaverseRoyal />} />
+
+        <Route exact path="/ar-development" element={<ARdevelopment />} />
+
 
 
 
