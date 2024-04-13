ba5990fa237a55655a8db5e45e41cbf3e16dfd2b
pratheeshraj <rajeshnsk5@gmail.com>
2024-04-13 18:45:51 +0530

    add

--- src/components/unityGame/UnityAbout.jsx
+++ src/components/unityGame/UnityAbout.jsx
@@ -24,26 +24,28 @@ const UnityAbout = () => {
           <div className="col-lg-6">
             <div className="right-wrapper ps-2">
               <h2 style={{ color: "black" }} className="title">
-                Our Expertise In Unity Development
+                Our Expertise In Unity Game Development
               </h2>
               <p style={{ color: "black" }}>
-                we have honed our skills to deliver innovative and immersive
-                experiences across various platforms. Our expertise encompasses:
+                We have honed our skills to deliver innovative and immersive
+                experiences across various platforms.
               </p>
               <p>
-                Comprehensive Understanding: We possess an in-depth
-                understanding of the Unity platform, leveraging its capabilities
-                to their fullest extent in every project.
+                <strong> Comprehensive Understanding:</strong> We possess an
+                in-depth understanding of the Unity platform, leveraging its
+                capabilities to their fullest extent in every project.
               </p>
               <p>
-                Versatility Across Genres: Whether it's mobile games, AR/VR
-                applications, simulations, or interactive experiences, our team
-                has the versatility to excel in diverse genres and mediums.
+                <strong> Versatility Across Genres: </strong> Whether it's
+                mobile games, AR/VR applications, simulations, or interactive
+                experiences, our team has the versatility to excel in diverse
+                genres and mediums.
               </p>
               <p>
-                Creative Excellence: Creativity is at the heart of everything we
-                do. From concept ideation to execution, we strive to push
-                boundaries and deliver experiences that captivate and inspire.
+                <strong>Creative Excellence:</strong> Creativity is at the heart
+                of everything we do. From concept ideation to execution, we
+                strive to push boundaries and deliver experiences that captivate
+                and inspire.
               </p>
             </div>
           </div>
