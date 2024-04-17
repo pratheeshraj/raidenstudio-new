3a76c6aef2e419a632e523dd84065db84dd127d1
Nowsath04 <nowsathnichu@gmail.com>
2024-04-17 20:57:48 +0530

    added

--- src/components/AI-As-Sevice/AsService.jsx
+++ src/components/AI-As-Sevice/AsService.jsx
@@ -15,42 +15,42 @@ const AsService = () => {
                     </p>
                     <div className="auto-container row key_features_section_container" style={{ rowGap: "29px" }}>
                         <div style={{ background: "white", boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px", borderRadius: "8px", width: "340px", height: "320px" }} className="key_features_card">
-                            <img src="/assets/img/raidensimx/key features/immersive.svg" alt="" />
+                            <img src="/assets/img/ai-as-empowering/scalable.svg" alt="" />
                             <h6 style={{ color: "black" }}>Scalable AI Infrastructure</h6>
                             <p style={{ color: "black", marginBottom: "0" }}>
                                 Access to scalable computing resources tailored to your AI workload, ensuring optimal performance and cost-effectiveness.
                             </p>
                         </div>
                         <div style={{ background: "white", boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px", borderRadius: "8px", width: "340px", height: "320px" }} className="key_features_card">
-                            <img src="/assets/img/raidensimx/key features/immersive.svg" alt="" />
+                            <img src="/assets/img/ai-as-empowering/aimodels.svg" alt="" />
                             <h6 style={{ color: "black" }}>Pre-built AI Models</h6>
                             <p style={{ color: "black", marginBottom: "0" }}>
                                 Choose from a range of pre-trained AI models spanning various domains, accelerating your time-to-market for AI-powered solutions.
                             </p>
                         </div>
                         <div style={{ background: "white", boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px", borderRadius: "8px", width: "340px", height: "320px" }} className="key_features_card">
-                            <img src="/assets/img/raidensimx/key features/immersive.svg" alt="" />
+                            <img src="/assets/img/ai-as-empowering/aimodeldev.svg" alt="" />
                             <h6 style={{ color: "black" }}>Custom Model Development</h6>
                             <p style={{ color: "black", marginBottom: "0" }}>
                                 Tailor-made AI model development services to address your specific business needs, from data preprocessing to model deployment.
                             </p>
                         </div>
                         <div style={{ background: "white", boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px", borderRadius: "8px", width: "340px", height: "320px" }} className="key_features_card">
-                            <img src="/assets/img/raidensimx/key features/immersive.svg" alt="" />
+                            <img src="/assets/img/ai-as-empowering/api.svg" alt="" />
                             <h6 style={{ color: "black" }}>Robust APIs</h6>
                             <p style={{ color: "black", marginBottom: "0" }}>
                                 Seamless integration with your existing systems through robust APIs, enabling effortless incorporation of AI capabilities into your applications.
                             </p>
                         </div>
                         <div style={{ background: "white", boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px", borderRadius: "8px", width: "340px", height: "320px" }} className="key_features_card">
-                            <img src="/assets/img/raidensimx/key features/immersive.svg" alt="" />
+                            <img src="/assets/img/ai-as-empowering/support.svg" alt="" />
                             <h6 style={{ color: "black" }}>Ongoing Support</h6>
                             <p style={{ color: "black", marginBottom: "0" }}>
                                 Dedicated support and maintenance services to ensure the smooth operation of your AI solutions, with regular updates and enhancements.
                             </p>
                         </div>
                         <div style={{ background: "white", boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px", borderRadius: "8px", width: "340px", height: "320px" }} className="key_features_card">
-                            <img src="/assets/img/raidensimx/key features/immersive.svg" alt="" />
+                            <img src="/assets/img/ai-as-empowering/immersive.svg" alt="" />
                             <h6 style={{ color: "black" }}>NFT Development</h6>
                             <p style={{ color: "black", marginBottom: "0" }}>
                                 Our commitment extends beyond deployment, providing continuous support and updates to optimize your gaming experience.
