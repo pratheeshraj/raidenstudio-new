f8dbf980b412e8021597685437076ba664dd3102
Nowsath04 <nowsathnichu@gmail.com>
2024-04-17 20:22:29 +0530

    added

--- src/components/BlockChain/BlockchainIndustries.jsx
+++ src/components/BlockChain/BlockchainIndustries.jsx
@@ -10,27 +10,27 @@ function BlockchainIndustries() {
                     <p style={{ textAlign: "center", color: "white" }}>As a leading virtual reality app development company, we assist startups and established businesses from all sectors in fulfilling the potential of digital transformation via VR to boost revenue. </p>
                     <div className='feature_div'>
                         <div className='feature_card idustry'>
-                            <img style={{ marginLeft: "25px" }} src='/assets/img/meta-indust/defence.svg' />
+                            <img style={{ marginLeft: "25px" }} src='/assets/img/custom-industry/transport.svg' />
                             <div>Transportation
                             </div>
                         </div>
                         <div className='feature_card idustry'>
-                            <img src='/assets/img/meta-indust/defence.sv' />
+                            <img src='/assets/img/custom-industry/cybersecuriity.svg' />
                             <div>Cybersecurity
                             </div>
                         </div>
                         <div className='feature_card idustry'>
-                            <img src='/assets/img/meta-indust/defence.sv' />
+                            <img src='/assets/img/custom-industry/enterprise.svg' />
                             <div>Enterprise
                             </div>
                         </div>
                         <div className='feature_card idustry'>
-                            <img src='/assets/img/meta-indust/defence.sv' />
+                            <img src='/assets/img/custom-industry/supplychai.svg' />
                             <div>Supply Chain
                             </div>
                         </div>
                         <div className='feature_card idustry'>
-                            <img src='/assets/img/meta-indust/defence.sv' />
+                            <img src='/assets/img/custom-industry/iot.svg' />
                             <div>IoT </div>
                         </div>
                         <div className='feature_card idustry'>
@@ -38,11 +38,11 @@ function BlockchainIndustries() {
                             <div>Social Networking</div>
                         </div>
                         <div className='feature_card idustry'>
-                            <img src='/assets/img/meta-indust/defence.sv' />
+                            <img src='/assets/img/custom-industry/telecommuni.svg' />
                             <div>Telecom</div>
                         </div>
                         <div className='feature_card idustry'>
-                            <img src='/assets/img/meta-indust/defence.sv' />
+                            <img src='/assets/img/custom-industry/fintech.svg' />
                             <div>Fintech</div>
                         </div>
                         <div className='feature_card idustry'>
@@ -50,11 +50,11 @@ function BlockchainIndustries() {
                             <div>Health & Fitness</div>
                         </div>
                         <div className='feature_card idustry'>
-                            <img src='/assets/img/meta-indust/defence.sv' />
+                            <img src='/assets/img/custom-industry/stock.svg' />
                             <div>Stock Trading</div>
                         </div>
                         <div className='feature_card idustry'>
-                            <img src='/assets/img/meta-indust/defence.sv' />
+                            <img src='/assets/img/custom-industry/insurance.svg' />
                             <div>Insurance</div>
                         </div>
                         <div className='feature_card idustry'>
