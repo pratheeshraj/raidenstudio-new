73e190916900fa6f10e5bc7a4474946a1b1f9a7c
pratheeshraj <rajeshnsk5@gmail.com>
2024-04-23 13:59:07 +0530

    add

--- src/elements/HelmetReact.jsx
+++ src/elements/HelmetReact.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-const HelmetReact = ({ title }) => {
+const HelmetReact = ({ title, description, keywords, ogimage }) => {
   return (
     <Helmet>
-      <title>
-        {title} || raidenStudio
-      </title>
-      <meta
-        name="description"
-        content="raidenStudio"
-      />
+      <title>{title} || raidenStudio</title>
+      
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <meta property="og:image" content={ogimage} />
     </Helmet>
   );
 };
