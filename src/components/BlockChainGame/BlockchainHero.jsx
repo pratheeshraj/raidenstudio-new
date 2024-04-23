e19a2dcce4e760993c0036f41e95a754fa53eb9a
Nowsath04 <nowsathnichu@gmail.com>
2024-04-23 20:32:05 +0530

    added

--- src/components/BlockChainGame/BlockchainHero.jsx
+++ src/components/BlockChainGame/BlockchainHero.jsx
@@ -30,7 +30,7 @@ const BlockchainHero = () => {
                                     className="slide-bg"
                                     style={{
                                         backgroundImage:
-                                            "url('assets/img/bg-image/user-engages-with-futuristic-vr-technology-digital-cyberspace_1151092-683-transformed.jpeg')",
+                                            "url('assets/img/blockgame/hero.jpg')",
                                         height: "100%"
                                     }}
                                 />
