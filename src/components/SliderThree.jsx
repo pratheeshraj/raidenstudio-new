12a29a1eabc53b45e514df8e31fa306f6fdc9726
pratheeshraj <rajeshnsk5@gmail.com>
2024-05-02 15:45:34 +0530

    add

--- src/components/SliderThree.jsx
+++ src/components/SliderThree.jsx
@@ -1,258 +1,114 @@
-import React, { useEffect, useRef } from "react";
-import SwiperCore, {
-  Autoplay,
-  Navigation,
-  FreeMode,
-  Thumbs,
-  EffectFade,
-} from "swiper";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import ParticlesComponent from "../ParticlesComponent/particles";
-import section3_img from "../images/barberian.png";
-// Import Swiper styles
-import "swiper/css"; // Import core styles
-import "swiper/css/navigation"; // Import navigation styles
-import "swiper/css/effect-fade"; // Import fade effect styles
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
-import ScrollAnimation from "react-animate-on-scroll";
 
-// Install Swiper modules
-SwiperCore.use([Autoplay, Navigation, FreeMode, Thumbs, EffectFade]);
+function SliderThree() {
+ 
+  const items = [
+    {
+      imgSrc: "/assets/img/bg-image/ai-new-home.jpg",
+      title: `Supercharge with <br><span style="color:#2ac8fd" >Artificial Intelligence</span>`,
+      description:
+        "Explore the forefront of innovation with our AI development <br> expertise.Transforming possibilities into tangible achievements.",
+        color:"#2ac8fd"
+    },
+    {
+      imgSrc: "/assets/img/bg-image/Gaming-barb (1).jpg",
+      title: `Forge Your Gaming <br> <span style="color:#13c4a1">Legacy</span>`,
+      description:
+        "Immerse Yourself in the Art of Gaming Creation Let's <br> Build Your Next Masterpiece Together!",
+        color:"#13c4a1"
+      
+    },
+    {
+      imgSrc: "/assets/img/bg-image/metaverse.jpg",
+      title: `Redefining Experiences <br> <span style="color:#2ac8fd">with XR</span>`,
+      description:
+        "Discover augmented and virtual realities with our <br> expert XR development.",
+        color:"#2ac8fd"
+    },
+    {
+      imgSrc: "/assets/img/bg-image/technonogy.jpg",
+      title: `Leading Blockchain <br> <span style="color:#13c4a1">Solutions Await</span>`,
+      description:
+        "Revolutionize with Cutting-Edge Blockchain Solutions<br>Unleash the power of innovation for your business.",
+        color:"#13c4a1"
+    },
+  ];
+  const [itemActive, setItemActive] = useState(0);
+  const intervalRef = useRef(null);
+  const sliderRef = useRef(null);
+  const countItem = items.length;
 
-const SliderThree = () => {
-  const sectionRef = useRef(null);
-  const triggerRef = useRef(null);
+  const handleNext = () => {
+    setItemActive((prev) => (prev + 1) % countItem);
+    sliderRef.current.classList.add("next");
+  };
 
-  // useEffect(() => {
-  //     const pin = gsap.fromTo(
-  //         sectionRef.current,
-  //         {
-  //             translateX: 0,
-  //         },
-  //         {
-  //             translateX: "-300vw",
-  //             ease: "none",
-  //             duration: 1,
-  //             scrollTrigger: {
-  //                 trigger: triggerRef.current,
-  //                 start: "top top",
-  //                 end: "2000 top",
-  //                 scrub: 0.6,
-  //                 pin: true,
-  //             },
-  //         }
-  //     );
-  //     return () => {
-  //         {
-  //             // / A return function for killing the animation on component unmount /
-  //         }
-  //         pin.kill();
-  //     };
-  // }, []);
+  const handlePrev = () => {
+    setItemActive((prev) => (prev - 1 + countItem) % countItem);
+    sliderRef.current.classList.add("prev");
+  };
+
+  const selectItem = (index) => {
+    setItemActive(index);
+  };
+
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(handleNext, 8000);
+    return () => clearInterval(intervalRef.current);
+  }, [handleNext]);
 
-    const sections = gsap.utils.toArray(".scroll-section");
+  useEffect(() => {
+    const slider = sliderRef.current;
 
-    gsap.to(sections, {
-      xPercent: -100 * (sections.length - 1),
-      ease: "none",
-      scrollTrigger: {
-        trigger: sectionRef.current,
-        pin: true,
-        scrub: 1,
-        snap: 1 / (sections.length - 1),
-
-        end: () => `+=${triggerRef?.current?.offsetWidth}`,
-      },
-    });
-    return () => {
-      ScrollTrigger.getAll().forEach((instance) => instance.kill());
-      gsap.killTweensOf("*");
+    const handleAnimationEnd = () => {
+      if (slider.classList.contains("next")) {
+        slider.classList.remove("next");
+      } else if (slider.classList.contains("prev")) {
+        slider.classList.remove("prev");
+      }
     };
-  }, []);
+
+    slider.addEventListener("animationend", handleAnimationEnd, { once: true });
+
+    return () => {
+      slider.removeEventListener("animationend", handleAnimationEnd);
+    };
+  }, [itemActive]);
+
   return (
     <>
-      <section className="scroll-section-outer">
-        <div ref={triggerRef}>
-          <div ref={sectionRef} className="scroll-section-inner">
-            <div className="scroll-section section1">
-
-              {" "}
-              <div className="auto-container section1_container">
-                <div
-                  animateIn="fadeInLeft"
-                  duration={1.5}
-                  className="left"
-                >
-                  <h2>
-                    Leading Blockchain
-                    <span> Solutions Await</span>
-                  </h2>
-                  <p className="Hero_sub_heading">
-                    Revolutionize with Cutting-Edge Blockchain Solutions <br /> Unleash the power of innovation for your business.
-                  </p>
-                  <Link className="default-btn" to="/contact-us">
-                    Explore
-                  </Link>
-                </div>
-              </div>
-
-            </div>
-            <div className="scroll-section section2">
-              <div className="auto-container section2_container">
-                <div className="left" data-aos="fade-left" data-aos-delay="300">
-                  <h2 className="slider-title">
-                    Supercharge with
-                    <span style={{ color: "#2ac8fd" }} className="theme-color">
-                      {" "}
-                      Artificial Intelligence
-                    </span>
-                  </h2>
-                  <p className="Hero_sub_heading description">
-                    Explore the forefront of innovation with our AI development <br /> expertise. Transforming possibilities into tangible achievements.
-                  </p>
-                  <Link
-                    style={{ backgroundColor: "#2ac8fd" }}
-                    className="default-btn"
-                    to="/contact-us"
-                  >
-                    Explore
-                  </Link>
-                </div>
+      <div className="slider" ref={sliderRef} >
+        <div className="list">
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className={`item ${index === itemActive ? "active" : ""}`}
+            >
+              <img src={item.imgSrc} alt={`Slide ${index}`} />
+              <div className="auto-container content">
+                <h2 dangerouslySetInnerHTML={{ __html: item.title }}></h2>
+                <p dangerouslySetInnerHTML={{ __html: item.description }}></p>
+                <Link className="default-btn" style={{backgroundColor:item.color}}>Explore</Link>
               </div>
             </div>
-            <div className="scroll-section section3">
-              <div className="auto-container section3_container">
-                <div className="left">
-                  <h2 className="slider-title">
-                    Forge Your Gaming
-                    <span className="theme-color"> Legacy </span>
-                  </h2>
-                  <p className="description Hero_sub_heading" >
-                    Immerse Yourself in the Art of Gaming Creation - Let's<br /> Build Your Next Masterpiece Together!
-                  </p>
-                  <Link className="default-btn" to="/contact-us">
-                    Explore
-                  </Link>
-                </div>
-                <div className="right">
-                  <img src={section3_img} alt="" />
-                </div>
-              </div>
-            </div>
-            <div className="scroll-section section4">
-              <div
-                className="auto-container section4_container"
-                data-aos="fade-left"
-                data-aos-delay="300"
-              >
-                <div className="left">
-                  <h2 className="slider-title">Redefining Experiences<span className="theme-color" style={{ color: "#5CF8FF" }}>  with XR</span>
-                  </h2>
-                  <p className="description Hero_sub_heading">
-                    Discover augmented and virtual realities with our expert XR development.
-                  </p>
-                  <Link
-                    style={{ backgroundColor: "#00D1DB" }}
-                    className="default-btn"
-                    to="/contact-us"
-                  >
-                    Explore
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
-      </section>
+
+        <div className="thumbnail">
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className={`item ${index === itemActive ? "active" : ""}`}
+              onClick={() => selectItem(index)}
+            >
+              <img src={item.imgSrc} alt={`Thumbnail ${index}`} />
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
-};
-
+}
 export default SliderThree;
-
-// <div ref={triggerRef}>
-// <section className="hero_section" ref={sectionRef}>
-
-//     <div className="section-2">
-//       <div className="section_2_bg" />
-//       <div className="auto-container">
-//         <div className="slider-content-inner middle">
-//           <div className=" home_hero_section_content">
-//             <h4 className="sub-title">The Most Personalized</h4>
-//             <h2 className="slider-title">
-//               Best-in-Class Blockchain Development
-//             </h2>
-//             <p className="description">
-//               RAIDEN offers top-tier blockchain game development services
-//               tailored to gaming businesses of any size. Our team excels in
-//               NFT, Metaverse, and Sports Betting design and development,
-//               delivering exceptional and engaging gaming experiences.
-//             </p>
-//             <div className="inner-btn">
-//               <div>
-//                 <Link className="default-btn" to="/contact-us">
-//                   Contact Us Now
-//                 </Link>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//     <div className="section-3">
-//       <div className="section_3_bg" />
-//       <div className="auto-container " style={{height:"100%"}}>
-//           <div className=" home_hero_section_content section_three">
-//             <div className="left">
-//               <h4 className="sub-title">The Most Personalized</h4>
-//               <h2 className="slider-title">
-//                 Best-in-Class Blockchain Development
-//               </h2>
-//               <p className="description">
-//                 RAIDEN offers top-tier blockchain game development services
-//                 tailored to gaming businesses of any size. Our team excels
-//                 in NFT, Metaverse, and Sports Betting design and
-//                 development, delivering exceptional and engaging gaming
-//                 experiences.
-//               </p>
-//               <Link className="default-btn" to="/contact-us">
-//                 Contact Us Now
-//               </Link>
-//             </div>
-//             <div className="right">
-//               <img src={section3_img} alt="" />
-//             </div>
-//         </div>
-//       </div>
-//     </div>
-//     <div className="section-4">
-//       <div className="section_4_bg" />
-//       <div className="auto-container">
-//         <div className="slider-content-inner middle">
-//           <div className=" home_hero_section_content">
-//             <h4 className="sub-title">The Most Personalized</h4>
-//             <h2 className="slider-title">
-//               Best-in-Class Blockchain Development
-//             </h2>
-//             <p className="description">
-//               RAIDEN offers top-tier blockchain game development services
-//               tailored to gaming businesses of any size. Our team excels in
-//               NFT, Metaverse, and Sports Betting design and development,
-//               delivering exceptional and engaging gaming experiences.
-//             </p>
-//             <div className="inner-btn">
-//               <div>
-//                 <Link className="default-btn" to="/contact-us">
-//                   Contact Us Now
-//                 </Link>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   </section>
-// </div>
