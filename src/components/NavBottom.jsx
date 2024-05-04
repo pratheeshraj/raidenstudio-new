82737981d87903c01e94b798927aa9565bafedc1
Nowsath04 <nowsathnichu@gmail.com>
2024-05-04 11:52:14 +0530

    added

--- src/components/NavBottom.jsx
+++ src/components/NavBottom.jsx
@@ -7,13 +7,16 @@ function NavBottom() {
         <div className='NavBottom_main_div'>
             <div className='NavBottom_div'>
                 <Link className='NavBottom_content'>
-                    <i class="fa-solid fa-house icon_bottom"></i>
+                    <img src='\assets\img\about\RAIDEN LOGO.svg' />
                 </Link>
                 <Link className='NavBottom_content'>
-                    <i class="fa-solid fa-blog icon_bottom"></i>
+                    <i style={{ color: "#2FACEC" }} class="fa-brands fa-telegram icon_bottom"></i>
                 </Link>
                 <Link className='NavBottom_content'>
-                    <i class="fa-solid fa-address-card icon_bottom"></i>
+                    <i style={{ color: "#25D366" }} class="fa-brands fa-whatsapp icon_bottom"></i>
+                </Link>
+                <Link className='NavBottom_content'>
+                    <i style={{ color: "#2FACEC" }} class="fa-brands fa-skype icon_bottom"></i>
                 </Link>
             </div>
         </div>
