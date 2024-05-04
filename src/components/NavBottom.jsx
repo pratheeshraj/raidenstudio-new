7e3cd8a5d865d612054abc6ef2009134d25e9c22
Nowsath04 <nowsathnichu@gmail.com>
2024-05-04 18:34:24 +0530

    added

--- src/components/NavBottom.jsx
+++ src/components/NavBottom.jsx
@@ -9,13 +9,13 @@ function NavBottom() {
                 <Link to={"/"} className='NavBottom_content'>
                     <img src='\assets\img\about\RAIDEN LOGO.svg' />
                 </Link>
-                <Link to={""} className='NavBottom_content'>
+                <Link to={"skype:live:.cid.c468e07a8d611cab?chat"} className='NavBottom_content'>
                     <i style={{ color: "#2FACEC" }} class="fa-brands fa-telegram icon_bottom"></i>
                 </Link>
                 <Link to={"https://api.whatsapp.com/send?phone=919884036062&text=Hi,%20I%20need%20more%20details%20about%20Raiden%20software%20development%20services.%20Shall%20we%20discuss%20now?%20-%20https://raidenstudio.netlify.app/"} className='NavBottom_content'>
                     <i style={{ color: "#25D366" }} class="fa-brands fa-whatsapp icon_bottom"></i>
                 </Link>
-                <Link to={""} className='NavBottom_content'>
+                <Link to={"https://telegram.me/Raidenstudios"} target='blank' className='NavBottom_content'>
                     <i style={{ color: "#2FACEC" }} class="fa-brands fa-skype icon_bottom"></i>
                 </Link>
             </div>
